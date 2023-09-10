67f2000537a88a9a31c4cb3f303ddbc386c951ba
Corey Schroeder <codingcorey365@gmail.com>
2023-09-10 09:28:58 -0500

    comments? i think i hit save and saw that the file was changed

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -8,19 +8,37 @@
         double longerNumber = 3513516845321;
         decimal numberWithDot = 32.03m;
 
-
+        /*
+            When these are in "CLASS" you NEED to add "static"
+            to the data type. because it is NOT in the scope of the main.
+                    
+                    string dogName = "Ralph";
+                    int dogAge = 10;
+        */
+        
 
 
 
         static void Main(string[] args)
         {
-            string dogName = "Ralph";
-            int dogAge = 10;
+         /*
+            When these are in main you do not need to add "static"
+            to the data type. because it is in the scope of the main.
 
-                if (dogAge == 18)
-                {
-                    Console.WriteLine(dogName);
-                }
+                         string dogName = "Ralph";
+                         int dogAge = 10;
+
+         */
+
+         /*
+            Showed me a different concept of this being in main
+            not sure what the point of this particular example was.
+              
+                         if (dogAge == 18)
+                         {
+                         Console.WriteLine(dogName);
+                         }
+         */
 
             Console.WriteLine("Hello, World!");
             Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old");

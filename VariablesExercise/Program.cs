4353bf3e1763bf63ac1d577b3622e1f9e39e52e2
Corey Schroeder <codingcorey365@gmail.com>
2023-09-10 09:26:53 -0500

    variable declaration and assignment, compile error for static instance of object

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,30 @@
 ﻿namespace VariablesExercise
 {
     public class Program
-    {
+    { 
+        int number = 33;
+        char letter = 'a';
+        bool isCoderQuestionmark = false;
+        double longerNumber = 3513516845321;
+        decimal numberWithDot = 32.03m;
+
+
+
+
+
         static void Main(string[] args)
         {
+            string dogName = "Ralph";
+            int dogAge = 10;
+
+                if (dogAge == 18)
+                {
+                    Console.WriteLine(dogName);
+                }
+
             Console.WriteLine("Hello, World!");
+            Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old");
         }
     }
 }
+
